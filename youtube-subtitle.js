9d461c66b395d1635edcb121284f9389516115db
Dai Lei <szdailei@gmail.com>
2020-05-27 18:40:43 +0800

    Add bilingual subtitles

--- youtube-subtitle.js
+++ youtube-subtitle.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name               Youtube Subtitle
+// @name               自动进入Youtube剧场模式，打开中文字幕和解说词，记忆播放速度
 // @namespace    https://greasyfork.org
-// @version      1.1.3
-// @description  1. 打开中文字幕。2. 没有中文字幕则将第一个字幕翻译为简体中文。3. 翻译失败则使用第一个字幕。
+// @version      2.1.0
+// @description  自动进入Youtube剧场模式，汉语字幕位于播放器下方，解说词位于播放器右下侧。有字幕时，自动记忆设置的播放速度，重新进入Youtube不丢失；无字幕时，不自动调整播放速度。
 // @author      szdailei@gmail.com
 // @source      https://github.com/szdailei/GM-scripts
-// @match       https://www.youtube.com/watch?v=*
+// @match       https://www.youtube.com/*
 // @run-at       document-start
 // ==/UserScript==
 
@@ -13,30 +13,39 @@
 /**
 require:  Trigger the event yt-navigate-finish. That's a special event in www.youtube.com, happens when open link in an exsit tab.
 ensure: 
-  Meet one of the conditions as the following order:
-    1. If there is Chinese subtitle, turn on it.
-    2. If there is non-Chinese subtitle and auto-translation, turn on the first subtitle and translate to Simp Chinese.
-    3. If there is non-Chinese subtitle without auto-translation, turn on the first subtitle.
-    4. If there isn't subtitle, doesn't turn on subtitle.
+    1. Open theater mode.
+    2. If the video is not played, or no subtitle, exit.
+    3. If there is Chinese subtitle, turn on it, and open transcript.
+    4. If there is non-Chinese subtitle and auto-translation, turn on the first subtitle and translate to Simp Chinese.
+    5. If there is non-Chinese subtitle without auto-translation, turn on the first subtitle.
+    6. If there is transcript, open transcript
+    7. If there is subtitle, save and restore play speed.
 */
-function youtubeSubtitle() {
-  var videoLoadCount, subtitleMenuLoadCount, translateToSimpChineseCount;
-  videoLoadCount = subtitleMenuLoadCount = translateToSimpChineseCount = 0;
+function onYtNavigateFinish() {
   const MAX_VIDEO_LOAD_COUNT = 10;
   const MAX_SUBTITLE_MENU_LOAD_COUNT = 10;
-  const MAX_TRANS_TO_SIMP_CHINESE_COUNT = 2;
-  const CHINESE_SUBTITLE = 'Chinese Subtitle';
-  const NO_SUBTITLE = 'No Subtitle';
-  const NON_CHINESE_SUBTITLE = 'Non Chinese Subtitle';
-  const ON_SUBTITLE_MENU = 'On Subtitle Menu';
+  const MAX_MORE_ACTIONS_MENU_LOAD_COUNT = 10;
+  const MAX_OPEN_TRANSCRIPT_COUNT = 10;
+  const PLAY_SPEED_LOCAL_STORAGE_KEY = 'greasyfork-org-youtube-subtitle-play-speed';
 
-  function onYtNavigateFinish() {
+  let playerSizeChanged = false;
+  let videoLoadCount, subtitleMenuLoadCount, moreActionsMenuLoadCount, openTranscriptCount;
+  videoLoadCount = subtitleMenuLoadCount = moreActionsMenuLoadCount = openTranscriptCount = 0;
+
+  if (window.location.pathname.indexOf('/watch') === -1) {
+    return;
+  }
+
+  // config on https://www.youtube.com/watch?*
+  youtubeConfig();
+
+  function youtubeConfig() {
     if (videoLoadCount === MAX_VIDEO_LOAD_COUNT) {
       return;
     }
-    var videos = document.getElementsByTagName('video');
+    let videos = document.getElementsByTagName('video');
     if (videos !== null) {
-      var video = videos.item(0);
+      let video = videos.item(0);
       if (video !== null) {
         video.play();
         onVideoPlayed();
@@ -44,23 +53,29 @@ function youtubeSubtitle() {
       }
     }
     videoLoadCount++;
-    setTimeout(onYtNavigateFinish, 1000);
+    setTimeout(youtubeConfig, 1000);
   }
 
   function onVideoPlayed() {
     if (subtitleMenuLoadCount === MAX_SUBTITLE_MENU_LOAD_COUNT) {
       return;
     }
-    var SubtitlesButtons = document.getElementsByClassName('ytp-subtitles-button');
-    if (SubtitlesButtons !== null) {
-      var SubtitlesButton = SubtitlesButtons[0];
+
+    if (playerSizeChanged === false) {
+      setPlayerFullSize();
+      playerSizeChanged = true;
+    }
+
+    let subtitlesButtons = document.getElementsByClassName('ytp-subtitles-button');
+    if (subtitlesButtons !== null) {
+      let subtitlesButton = subtitlesButtons[0];
       if (
-        SubtitlesButton !== undefined &&
-        SubtitlesButton !== null &&
-        SubtitlesButton.getAttribute('aria-pressed') !== null
+        subtitlesButton !== undefined &&
+        subtitlesButton !== null &&
+        subtitlesButton.getAttribute('aria-pressed') !== null
       ) {
-        if (SubtitlesButton.getAttribute('aria-pressed') === false) {
-          SubtitlesButton.click();
+        if (subtitlesButton.getAttribute('aria-pressed') === false) {
+          subtitlesButton.click();
         }
         onSubtitlesMenuLoaded();
         return;
@@ -70,92 +85,177 @@ function youtubeSubtitle() {
     setTimeout(onVideoPlayed, 1000);
   }
 
-  function onSubtitlesMenuLoaded() {
-    var turnOnChineseSubtitleResult = turnOnChineseSubtitle();
-    clickSettingsButtion();
-    if (turnOnChineseSubtitleResult === NON_CHINESE_SUBTITLE) {
-      translateToSimpChinese();
+  function setPlayerFullSize() {
+    let sizeButtons = document.getElementsByClassName('ytp-size-button');
+    if (sizeButtons !== null && sizeButtons.length === 1) {
+      let sizeButton = sizeButtons[0];
+      if (sizeButton.title.indexOf('剧场模式') !== -1) {
+        sizeButton.click();
+      }
     }
   }
 
-  function turnOnChineseSubtitle() {
-    clickSettingsButtion();
-    var enterSubtitleMenuResult = enterSubtitleMenu();
-    if (enterSubtitleMenuResult === CHINESE_SUBTITLE) {
+  function onSubtitlesMenuLoaded() {
+    turnOnSubtitle();
+    adjustPlaySpeed();
+  }
+
+  function turnOnSubtitle() {
+    clickSettingsButton();
+    enterSubMenu('字幕');
+    let result = getSubMenuButton('中文');
+    if (Array.isArray(result) === false) {
+      // turn on Chinese subtitle, and open transcript
+      result.click();
+      clickSettingsButton();
+      openTranscript();
       return;
     }
-    var menuItemRadios = document.querySelectorAll('[role="menuitemradio"]');
-    var length = menuItemRadios.length;
-    var firstSubtitleIndex = 0;
-    for (var i = 0; i < length; i++) {
-      var innerText = menuItemRadios[i].innerText;
-      if (innerText.indexOf('添加字幕') !== -1 || innerText.indexOf('关闭') !== -1) {
+
+    if (result.includes('自动翻译') === true) {
+      // translate to Simp Chinese subtitle, and open transcript
+      turnOnAutoTrans();
+      openTranscript();
+      return;
+    }
+
+    let length = result.length;
+    for (let i = 0; i < length; i++) {
+      let buttonName = result[i];
+      if (buttonName.indexOf('添加字幕') !== -1 || buttonName.indexOf('关闭') !== -1) {
         continue;
       }
-      firstSubtitleIndex = i;
-      if (innerText.indexOf('中文') !== -1) {
-        menuItemRadios[i].click();
-        return CHINESE_SUBTITLE;
-      }
-    }
-    if (firstSubtitleIndex === 0) {
-      return NO_SUBTITLE;
-    } else {
-      menuItemRadios[firstSubtitleIndex].click();
-      return NON_CHINESE_SUBTITLE;
-    }
-  }
-
-  function translateToSimpChinese() {
-    if (translateToSimpChineseCount === MAX_TRANS_TO_SIMP_CHINESE_COUNT) {
+      // turn on non Chinese subtitle, and open transcript
+      getSubMenuButton(buttonName).click();
+      clickSettingsButton();
+      openTranscript();
       return;
     }
-    clickSettingsButtion();
-    enterSubtitleMenu();
-    var menuItemRadios = document.querySelectorAll('[role="menuitemradio"]');
-    var length = menuItemRadios.length;
-    if (menuItemRadios[length - 1].innerText === '自动翻译') {
-      menuItemRadios[length - 1].click();
-      menuItemRadios = document.querySelectorAll('[role="menuitemradio"]');
-      length = menuItemRadios.length;
-      for (var i = length - 1; i > -1; i--) {
-        var innerText = menuItemRadios[i].innerText;
-        if (innerText.indexOf('中文（简体）') !== -1) {
-          menuItemRadios[i].click();
-          return;
-        }
+    clickSettingsButton();
+  }
+
+  function clickSettingsButton() {
+    let settingsButtons = document.getElementsByClassName('ytp-settings-button');
+    let settingsButton = settingsButtons[0];
+
+    settingsButton.click();
+  }
+
+  function enterSubMenu(menuName) {
+    let menuItems = document.querySelectorAll('[role="menuitem"]');
+    let length = menuItems.length;
+    for (let i = 0; i < length; i++) {
+      let innerText = menuItems[i].innerText;
+      if (innerText.indexOf(menuName) !== -1) {
+        menuItems[i].click();
+        return;
       }
-      clickSettingsButtion();
+    }
+  }
+
+  function getSubMenuButton(buttonName, exactMatch) {
+    let buttonNames = [];
+    let menuItemRadios = document.querySelectorAll('[role="menuitemradio"]');
+    let length = menuItemRadios.length;
+    for (let i = length - 1; i > -1; i--) {
+      let innerText = menuItemRadios[i].innerText;
+      if (exactMatch === true) {
+        if (innerText === buttonName) {
+          return menuItemRadios[i];
+        }
+      } else if (innerText.indexOf(buttonName) !== -1) {
+        return menuItemRadios[i];
+      }
+      buttonNames.push(innerText);
+    }
+    return buttonNames;
+  }
+
+  function turnOnAutoTrans() {
+    let autoTransButton = getSubMenuButton('自动翻译');
+    autoTransButton.click();
+    let simpChineseButton = getSubMenuButton('中文（简体）');
+    simpChineseButton.click();
+  }
+
+  function openTranscript() {
+    if (moreActionsMenuLoadCount === MAX_MORE_ACTIONS_MENU_LOAD_COUNT) {
       return;
     }
-    clickSettingsButtion();
-    translateToSimpChineseCount++;
-    setTimeout(translateToSimpChinese, 1000);
+
+    let result = clickMoreActionsMenuButton();
+    if (result === true) {
+      clickOpenTranscriptButton();
+      return;
+    }
+    moreActionsMenuLoadCount++;
+    setTimeout(openTranscript, 1000);
   }
 
-  function clickSettingsButtion() {
-    var settingsButtions = document.getElementsByClassName('ytp-settings-button');
-    var settingsButtion = settingsButtions[0];
-    settingsButtion.click();
-  }
-
-  function enterSubtitleMenu() {
-    var menuItems = document.querySelectorAll('[role="menuitem"]');
-    var length = menuItems.length;
-    for (var i = 0; i < length; i++) {
-      var innerText = menuItems[i].innerText;
-      if (innerText.indexOf('字幕') !== -1) {
-        if (innerText.indexOf('中文') !== -1) {
-          return CHINESE_SUBTITLE;
-        } else {
-          menuItems[i].click();
-          return ON_SUBTITLE_MENU;
+  function clickMoreActionsMenuButton() {
+    let dropdownTriggers = document.getElementsByClassName('dropdown-trigger');
+    if (dropdownTriggers !== null) {
+      let length = dropdownTriggers.length;
+      for (let i = 0; i < length; i++) {
+        let button = dropdownTriggers[i].children[0];
+        if (button !== null && button.getAttribute('aria-label') === '其他操作') {
+          button.click();
+          return true;
         }
       }
     }
   }
-  
-  onYtNavigateFinish();
+
+  function clickOpenTranscriptButton() {
+    if (openTranscriptCount === MAX_OPEN_TRANSCRIPT_COUNT) {
+      return;
+    }
+    let menuItems = document.querySelectorAll('[role="option"]');
+    if (menuItems !== null && menuItems.length >= 2) {
+      if (menuItems[1].innerText.indexOf('打开解说词') !== -1) {
+        menuItems[1].click();
+        return;
+      } else {
+        // Timeout, the more actions menu closed automaticly. Turn on again.
+        clickMoreActionsMenuButton();
+      }
+    }
+    openTranscriptCount++;
+    setTimeout(clickOpenTranscriptButton, 1000);
+  }
+
+  function adjustPlaySpeed() {
+    clickSettingsButton();
+    enterSubMenu('播放速度');
+    listenPlaySpeedButtonClick();
+
+    let playSpeedInLocalStorage = localStorage.getItem(PLAY_SPEED_LOCAL_STORAGE_KEY);
+    if (playSpeedInLocalStorage === null) {
+      clickSettingsButton();
+      return;
+    }
+
+    // restore play speed
+    const exactMatch = true;
+    let result = getSubMenuButton(playSpeedInLocalStorage, exactMatch);
+    if (Array.isArray(result) === false) {
+      result.click();
+    }
+    clickSettingsButton();
+  }
+
+  function listenPlaySpeedButtonClick() {
+    let menuItemRadios = document.querySelectorAll('[role="menuitemradio"]');
+    let length = menuItemRadios.length;
+    for (let i = 0; i < length; i++) {
+      menuItemRadios[i].addEventListener('click', savePlaySpeed);
+    }
+  }
+
+  function savePlaySpeed() {
+    let playSpeed = this.innerText;
+    localStorage.setItem(PLAY_SPEED_LOCAL_STORAGE_KEY, playSpeed);
+  }
 }
 
 /**
@@ -163,5 +263,5 @@ require:  The document is still loading
 ensure:  addEventListener on yt-navigate-finish event.
 */
 (function () {
-  window.addEventListener('yt-navigate-finish', youtubeSubtitle);
+  window.addEventListener('yt-navigate-finish', onYtNavigateFinish);
 })();
